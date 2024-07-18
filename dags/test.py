7e85b9e2adccf8b2bca0ca01013f95822b54a8bf
nantawat chairat <nantawat@opsta.co.th>
2024-07-19 01:11:49 +0700

    feat: Add docker-compose configuration for Airflow
    chore: Update AIRFLOW_CONFIG path in docker-compose.yaml

---
+++ dags/test.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+
+default_args = {
+    'owner': 'LDB',
+}
+
+
+def echo_datetime():
+    current_datetime = datetime.now()
+    print(f"The current datetime is: {current_datetime}")
+
+
+with DAG(
+    'test',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval='@daily',
+    default_args=default_args,
+) as dag:
+    task_echo_datetime = PythonOperator(
+        task_id='echo_datetime',
+        python_callable=echo_datetime
+    )
